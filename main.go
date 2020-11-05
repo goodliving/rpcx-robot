ca24bbba8b9e6340a13929e0f2b711fd0e220e00
pzm <pzm@servyou.local>
2020-11-05 17:09:33 +0800

    inti

--- main.go
+++ main.go
@@ -12,9 +12,55 @@ func main()  {
 		log.Fatal(err)
 	}
 
-	root := doc.SelectElement("robot")
-	suite := root.SelectElement("suite")
-	for _, value := range suite.Child {
-		fmt.Println("value: ", value)
+	suite := doc.SelectElement("robot")
+	getTestInfo(suite)
+
+
+	//fmt.Println("suite: ", suite)
+}
+
+func getTestInfo(e *etree.Element) {
+
+	suites := e.SelectElements("suite")
+	tests := e.SelectElements("test")
+
+	if len(tests) != 0 {
+		for _, test := range tests {
+			for _, value := range test.Parent().Attr {
+				if value.Key == "source" {
+					fmt.Println("module: ", value.Value)
+				}
+			}
+
+			for _, value := range test.Attr {
+				if value.Key == "name" {
+					fmt.Println("testName: ", value.Value)
+				}
+			}
+
+			testStatus := test.SelectElement("status")
+			for _, value := range testStatus.Attr {
+				if value.Key == "status" {
+					fmt.Println("testStatus: ", value.Value)
+
+					if value.Value == "FAIL" {
+						msg := testStatus.SelectElement("msg")
+						fmt.Println("msg: ", msg)
+					}
+				}
+
+				if value.Key == "critical" {
+					fmt.Println("critical: ", value.Element().Text())
+				}
+			}
+		}
+	}
+
+	if len(suites) == 0 {
+		return
+	}
+
+	for _, v := range suites {
+		getTestInfo(v)
 	}
 }
